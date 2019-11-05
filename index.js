4a301158cc6fed2723710b7f4612a22e90ed8e46
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-11-05 18:41:53 +0100

    avoid rAF/cAF per each invoke => performance

--- index.js
+++ index.js
@@ -5,26 +5,34 @@ var reraf = (function (exports) {
   var cAF = compat ? cancelAnimationFrame : clearTimeout;
   var rAF = compat ? requestAnimationFrame : setTimeout;
   function reraf(limit) {
-    var force, timer;
-    return reset(), function reschedule(callback, self, args) {
-      cAF(timer);
-      if (--force < 0)
-        invoke();
-      else
+    var force, timer, callback, self, args;
+    reset();
+    return function reschedule(_callback, _self, _args) {
+      callback = _callback;
+      self = _self;
+      args = _args;
+      if (!timer)
         timer = rAF(invoke);
-      return function stop(flush) {
-        cAF(timer);
-        if (flush && timer)
-          invoke();
-      };
-      function invoke() {
-        reset();
-        callback.apply(self, args || []);
-      }
+      if (--force < 0)
+        stop(true);
+      return stop;
     };
+    function invoke() {
+      reset();
+      callback.apply(self, args || []);
+    }
     function reset() {
       force = limit || Infinity;
-      timer = 0;
+      timer = compat ? 0 : null;
+    }
+    function stop(flush) {
+      var didStop = !!timer;
+      if (didStop) {
+        cAF(timer);
+        if (flush)
+          invoke();
+      }
+      return didStop;
     }
   }
 
